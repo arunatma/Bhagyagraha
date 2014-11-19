8d6e31d1788cbfbeee9d968503d02c34b3af68fe
Arunram A <arunram.atma@gmail.com>
2014-11-20 00:07:26 +0530

    Top level definition for astro.py

--- astro.py
+++ astro.py
@@ -1,13 +1,4 @@
-import functions as fn
-import constants as cn
-import datetime as dt
-    
-def test_tamil_date():
-    input_params = fn.get_input()
-    d, m, y = fn.calc_tamil_date(input_params["in_datetime"])
-    print d
-    print m
-    print y
+# This file will have wrapper functions
     
     
     
